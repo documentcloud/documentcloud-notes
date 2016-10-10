e90050794c75d79b5119f3666076f2bd336ac29a
Justin Reese <justin@justinreese.com>
2016-10-10 17:00:25 -0500

    Rewrite loader to be asynchronous
    * Insert `note_embed.js` using `document.createElement` instead of `document.write`, and flag it `async`
    * Only write one copy of `note_embed.(js|css)` to page
    * Queue up notes to execute after `note_embed.js` is availble
    * Add nuclear option for creating the target DIV for a note that has an inline script but no container
    
    # Conflicts:
    #	dist/note_embed.js

--- test/loader.js
+++ test/loader.js
@@ -1,31 +1,69 @@
 (function() {
-  // If the note embed is already loaded, don't repeat the process.
-  if (window.dc) { if (window.dc.noteEmbedLoaded) { return; } }
-
-  window.dc = window.dc || {};
+  window.dc           = window.dc || {};
   window.dc.recordHit = "//www.documentcloud.org/pixel.gif";
+  window.dc.noteQueue = window.dc.noteQueue || [];
+  window.dc.embed     = window.dc.embed     || {};
 
-  var loadCSS = function(url, media) {
-    var link   = document.createElement('link');
-    link.rel   = 'stylesheet';
-    link.type  = 'text/css';
-    link.media = media || 'screen';
-    link.href  = url;
-    var head   = document.getElementsByTagName('head')[0];
-    head.appendChild(link);
-  };
+  // Public entry point
+  window.dc.embed.loadNote = function(url, options) {
+    if (window.dc.noteEmbedJSParsed) {
+      // We already have access to `note_embed.js`, so just proxy on to the 
+      // real note-loader
+      window.dc.embed.actuallyLoadNote(url, options);
+    } else {
 
-  /*@cc_on
-  /*@if (@_jscript_version < 5.8)
-    loadCSS("../dist/note_embed.css");
-  @else @*/
-    loadCSS("../dist/note_embed-datauri.css");
-  /*@end
-  @*/
+      // 1. Define the note-loading utilities we'll need
+      var noteLoadingUtils = {
 
-  // Record the fact that the note embed is loaded.
-  dc.noteEmbedLoaded = true;
+        samizdatOn: function (el, eventName, handler) {
+          if (el.addEventListener) {
+            el.addEventListener(eventName, handler);
+          } else {
+            el.attachEvent('on' + eventName, function() {
+              handler.call(el);
+            });
+          }
+        },
 
-  // Request the embed JavaScript.
-  document.write('<script type="text/javascript" src="../dist/note_embed.js"></scr' + 'ipt>');
+        insertStylesheet: function(href) {
+          if (!document.querySelector('link[href$="' + href + '"]')) {
+            stylesheet   = document.createElement('link');
+            stylesheet.rel   = 'stylesheet';
+            stylesheet.type  = 'text/css';
+            stylesheet.media = 'screen';
+            stylesheet.href  = href;
+            document.querySelector('head').appendChild(stylesheet);
+          }
+        },
+
+        insertJavaScript: function(src, onLoadCallback) {
+          if (!document.querySelector('script[src$="' + src + '"]')) {
+            script = document.createElement('script');
+            script.src = src;
+            script.async = true;
+            this.samizdatOn(script, 'load', onLoadCallback);
+            document.querySelector('body').appendChild(script);
+          }
+        },
+
+        loadNotes: function() {
+          q = window.dc.noteQueue;
+          for (var i = 0, qLength = q.length; i < qLength; i++) {
+            window.dc.embed.actuallyLoadNote(q[i].url, q[i].options);
+          }
+          window.dc.noteQueue = [];
+        },
+
+      };
+
+      // 2. Add this note to the queue that `loadNotes` will use
+      window.dc.noteQueue.push({url: url, options: options});
+
+      // 3. Insert the styles and scripts needed, with `loadNotes` fired once 
+      // the script has finished loading
+      noteLoadingUtils.insertStylesheet('../dist/note_embed.css');
+      noteLoadingUtils.insertJavaScript('../dist/note_embed.js', noteLoadingUtils.loadNotes);
+    }
+  }
+
 })();
